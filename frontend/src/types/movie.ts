2c25832a09d0705a3cdd59757b7ccf7f14890cf9
Caio Menezes <caiofmen@hotmail.com>
2022-01-15 18:36:24 -0300

    useState, useEffect

--- frontend/src/types/movie.ts
+++ frontend/src/types/movie.ts
@@ -1,3 +1,19 @@
-import { isExportDeclaration } from "typescript";
+export type Movie = {
+    id: number;
+    title: string;
+    score: number;
+    count: number;
+    image: string;
+}
 
-isExportDeclaration
+export type MoviePage = {
+    content: Movie[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numberOfElements: number;
+    empty: boolean;
+}
